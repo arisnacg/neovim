8342984a531b1259e209b6138f1671f30747043d
Arisna <arisna@cakeplabs.com>
2023-06-09 15:23:13 +0800

    remove install.sh and lazy-lock.json

--- install.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-nvim_dir="$HOME/.config/nvim"
-if [ -d $nvim_dir ] 
-then
-  mv -f ~/.config/nvim ~/.config/nvim.backup
-fi
-mkdir ~/.config/nvim
-git clone https://github.com/arisnacg/neovim ~/.config/nvim
