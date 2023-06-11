991a39829045526911629285affe19f403faca4f
Arisna <arisna@cakeplabs.com>
2023-06-11 20:02:46 +0800

    update completion

--- lua/p-completion.lua
+++ lua/p-completion.lua
@@ -50,7 +50,7 @@ cmp.setup {
     select = false,
   },
   window = {
-    -- completion = cmp.config.window.bordered(),
+    completion = cmp.config.window.bordered(),
     documentation = false
   },
   experimental = {
